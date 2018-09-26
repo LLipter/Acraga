656db3ceb565347b027e36efd8a887a1b0811021
GGGWX <gggwx6198@yahoo.com>
2018-09-26 11:38:17 +0800

    some supplementary instructions

--- src/token/Identifier.java
+++ src/token/Identifier.java
@@ -14,9 +14,7 @@ public class Identifier extends Token {
         return id;
     }
 
-    public void setId(String id) {
-        this.id = id;
-    }
+    public void setId(String id) { this.id = id; }
 
     @Override
     public String toString() {
