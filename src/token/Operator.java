97e46e8c5f14c9ebdf79e2449f84023680e75ba9
LLran <katsudonn@163.com>
2018-10-17 10:27:46 +0800

    Staticlize operator

--- src/token/Operator.java
+++ src/token/Operator.java
@@ -10,17 +10,20 @@ import java.util.HashMap;
 public abstract class Operator extends ExpressionToken {
 
     protected OperatorType operatorType;
+    private static HashMap<OperatorType, Integer> priorityMap = new HashMap<OperatorType, Integer>();
+
+    static{
+        priorityMap.put(OperatorType.ADD, 1);
+        priorityMap.put(OperatorType.SUB, 1);
+        priorityMap.put(OperatorType.MUL, 2);
+        priorityMap.put(OperatorType.DIV, 2);
+    }
 
     public Operator() {
         tokenType = TokenType.OPERATOR;
     }
 
     public static boolean isPrioriThan(OperatorType type1, OperatorType type2) {
-        HashMap<OperatorType, Integer> priorityMap = new HashMap<OperatorType, Integer>();
-        priorityMap.put(OperatorType.ADD, 1);
-        priorityMap.put(OperatorType.SUB, 1);
-        priorityMap.put(OperatorType.MUL, 2);
-        priorityMap.put(OperatorType.DIV, 2);
         return priorityMap.get(type1) > priorityMap.get(type2);
     }
 
