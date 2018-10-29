02ab8d4940b5aa85c59c8254901d7fb7ca7e4723
LLran <katsudonn@163.com>
2018-10-29 14:54:22 +0800

    need debuging

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,16 +1,18 @@
 package main;
 
+import exception.RTException;
 import exception.SyntaxException;
 
 public class Main {
 
-    public static void main(String[] args) throws SyntaxException {
+    public static void main(String[] args) throws SyntaxException, RTException {
         // TODO Auto-generated method stub
 
         Preprocessor input = new Preprocessor("input1.acg");
         Scanner scanner = new Scanner(input);
         scanner.print();
         Parser parser=new Parser(scanner);
+        Interpreter interpreter = new Interpreter(parser);
 
     }
 
